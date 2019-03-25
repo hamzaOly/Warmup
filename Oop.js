f4492df2e708315d07b902d4b826f95b196ae6e7
hamzaOly <holimat6@gmail.com>
2019-03-25 09:33:37 +0200

    “OopSolved”

--- Oop.js
+++ Oop.js
@@ -6,25 +6,27 @@ function randInt(n) {
     return Math.floor(Math.random() * (n + 1));
 }
 
-function makeGame(upperbound){
-  var upper=upperbound;
-  var rand=randInt(upper)
-  var counter=0;
-  return {
-  	guessMyNumber:function(n){
-  		counter++;
-	    if (n > upper) {
-	      return "Out of bounds! Please try a number between 0 and " + upperbound + ".";
-	    } else if (n === rand) {
-	      return "You guessed my number!";
-	    }
-	    return "Nope! That wasn't it!";
-  	},
-  	giveUp:function(){
-    	return rand;
-  	},
-  	numOfGuesses:function(){
-    	return counter;
-  	}
+    function makeGame1(argument) {
+         var obj = {};
+           obj.upperbound= upperbound
+           obj.rand=randInt(upper)
+           obj.counter = 0;
+
+    guessMyNumber =function(n){
+         counter++;
+             if (n > upper) {
+                  return "Out of bounds! Please try a number between 0 and " + upperbound + ".";
+             } else if (n === rand) {
+                  return "You guessed my number!";
+      }
+                       return "Nope! That wasn't it!";
+    },
+         giveUp = function(){
+      return rand;
+    },
+          numOfGuesses =function(){
+      return counter;
+    }
+    return obj ; 
   }
-}
+
