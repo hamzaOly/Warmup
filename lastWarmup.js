d8b1ad64f17bf8562e9c74968273d9e602e0cdbf
RBK-21 <rbk-21@Mac-mini-5.Dlink>
2019-03-27 11:57:08 +0200

    lastWarmup.js solved

---
+++ lastWarmup.js
@@ -0,0 +1,44 @@
+/* 
+Given an array of integers, return indices of the two numbers
+such that they add up to a specific target.
+You may assume that each input would have exactly one solution.
+Example:
+var nums = [2, 7, 11, 15], target = 9,
+twoSum(nums,9); //should return [0,1] because nums[0] + nums[1] = 2 + 7 = 9;
+*/
+function each(coll, f) {
+	if (Array.isArray(coll)) {
+		for (var i = 0; i < coll.length; i++) {
+			f(coll[i], i);
+		}
+	} else {
+		for (var key in coll) {
+			f(coll[key], key);
+		}
+	}
+}
+
+ function reduce(array, f, acc) { 
+       if (acc === undefined) { 
+             acc = array[0]; 
+             array = array.slice(1); 
+       } 
+       each(array, function(element, i) { 
+             acc = f(acc, element, i); 
+       }); 
+       return acc; 
+ }
+
+
+function twoSum(sum, element) {
+	var array = [];
+	return reduce(sum , function(first){
+		return reduce(element, function(soc){
+        
+			return  array = sum[first[i]] + element[soc[i]]
+		})
+
+
+	})}
+
+
